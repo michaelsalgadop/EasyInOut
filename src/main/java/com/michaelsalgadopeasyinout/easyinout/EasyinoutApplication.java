3b34ed8d7e627ba725b808303e2cfb93e30598f7
Michaelsp11 <michaelsalgadop11@gmail.com>
2026-02-27 10:39:51 +0100

    Primer commit de EasyInOut

---
+++ src/main/java/com/michaelsalgadopeasyinout/easyinout/EasyinoutApplication.java
@@ -0,0 +1,13 @@
+package com.michaelsalgadopeasyinout.easyinout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyinoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyinoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelsalgadopeasyinout/easyinout/EasyinoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelsalgadopeasyinout.easyinout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyinoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
