3cfe1ee38b1b5f0a9cf38ad55a611582a9a76e59
Michaelsp11 <michaelsalgadop11@gmail.com>
2026-03-11 10:52:43 +0100

    feat: Creados repositorios por cada entidad

---
+++ src/main/java/com/michaelsalgadopeasyinout/easyinout/repositories/AbsenceTypeRepository.java
@@ -0,0 +1,9 @@
+package com.michaelsalgadopeasyinout.easyinout.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.michaelsalgadopeasyinout.easyinout.entities.AbsenceType;
+
+public interface AbsenceTypeRepository extends JpaRepository<AbsenceType, Long>{
+    
+}
